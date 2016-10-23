666ec1c2d5c5fa4c55e0b1dbd56c92fe847e5203
Jefferson <je_caetano@hotmail.com>
2016-10-23 20:50:10 -0200

    Números Primos

---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Preencha um número maior do que 0:");
+                int num = Convert.ToInt32(Console.ReadLine());
+                bool primo;
+                if (num < 1)
+                    throw new Exception();
+
+                List<int> numeros = new List<int>();
+
+                for (int i = 2; i <= num; i++)
+                {
+                    primo = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                            primo = false;
+                    }
+                    if (primo)
+                        numeros.Add(i);                    
+                }
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Números primos obtidos:");
+
+                for (int i = 0; i < numeros.Count; i++)
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+
+                int soma = numeros.Sum();
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Soma dos números primos: " + soma);
+
+                Console.WriteLine(" ");
+                foreach (int item in numeros)
+                {
+                    Console.WriteLine("Potência do número " + item + " por " + item + ": " + Math.Pow(item,item));
+                }
+
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Caracter Inválido");
+            }
+        }
+    }
+}
