1bd91dac2b34929d5d45dceb01f6644e38de87d5
Jefferson <je_caetano@hotmail.com>
2016-12-16 11:50:46 -0200

    Clonar objeto e utilizar enum

---
+++ CopiandoListaEmOutraLista/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopiandoListaEmOutraLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carroA = new Carro
+            {
+                Codigo = 1,
+                Marca = "Ferrari",
+                Preco = 300000,
+                AirBag = false
+            };
+
+            Carro carroB = new Carro
+            {
+                Codigo = 2,
+                Marca = "Porche",
+                Preco = 450000,
+                AirBag = true
+            };
+
+            List<Carro> listaCarros = new List<Carro>();
+            List<Carro> listaCarrosCopia = new List<Carro>();
+
+            listaCarros.Add(carroA);
+            listaCarros.Add(carroB);
+
+            
+            foreach (Carro item in listaCarros)
+                listaCarrosCopia.Add((Carro)ClonarObjeto(item));
+
+            //listaCarrosCopia = listaCarros;
+
+            listaCarrosCopia[0].Codigo = 3;
+
+            Console.WriteLine("listaCarrosCopia");
+            foreach (var item in listaCarrosCopia)
+            {
+                Console.WriteLine(string.Format("Código: {0}", item.Codigo));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("listaCarros");
+            foreach (var item in listaCarros)
+            {
+                Console.WriteLine(string.Format("Código: {0}", item.Codigo));
+            }
+
+            Console.ReadKey();
+        }
+        public static object ClonarObjeto(object objRecebido)
+        {
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                bf.Serialize(ms, objRecebido);
+                ms.Position = 0;
+
+                object obj = bf.Deserialize(ms);
+                ms.Close();
+
+                return obj;
+            }
+        }
+
+        [Serializable]
+        public class Carro
+        {
+            public int Codigo { get; set; }
+            public string Marca { get; set; }
+            public decimal Preco { get; set; }
+            public bool AirBag { get; set; }
+        }
+    }
+}
---
+++ EnumDescription/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace EnumDescription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro[] carros = new[] { Carro.Ferrari, Carro.Jaguar };
+            string texto = Carro.Ferrari.Descricao();
+            // texto = "Carro Ferrari"
+            if (carros.Any(c => c.Descricao() == texto))
+                Console.WriteLine("Correto");
+            else
+                Console.WriteLine("Incorreto");
+
+            // Resultado: Correto
+
+            Console.ReadKey();
+        }
+
+        public enum Carro
+        {
+            [Description("Carro Mercedes"), DefaultValue("M")]
+            Mercedes,
+            [Description("Carro Ferrari"), DefaultValue("F")]
+            Ferrari,
+            [Description("Carro Jaguar"), DefaultValue("J")]
+            Jaguar,
+            Porche
+        }        
+    }
+
+    public static class Util
+    {
+        public static string Descricao(this Enum itemEnum)
+        {
+            DescriptionAttribute descricao = (DescriptionAttribute)itemEnum.GetType().GetField(itemEnum.ToString()).
+                GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault();
+
+            string retorno = itemEnum.ToString();
+            if (descricao != null)
+                retorno = descricao.Description;
+            return retorno;
+        }
+
+        public static string ValorPadrao(this Enum itemEnum)
+        {
+            DefaultValueAttribute valorPadrao = (DefaultValueAttribute)itemEnum.GetType().GetField(itemEnum.ToString()).
+                GetCustomAttributes(typeof(DefaultValueAttribute), false).FirstOrDefault();
+            string retorno = itemEnum.ToString();
+            if (valorPadrao != null)
+                retorno = valorPadrao.Value.ToString();
+            return retorno;
+        }
+
+        public static T ReceberEnumDeDescricao<T>(this string descricao)
+        {
+            foreach (MemberInfo campo in (MemberInfo[])typeof(T).GetFields())
+            {
+                DescriptionAttribute[] atributoDescricao = (DescriptionAttribute[])campo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (atributoDescricao != null && atributoDescricao.Length > 0 && atributoDescricao[0].Description == descricao)
+                    return (T)Enum.Parse(typeof(T), campo.Name);
+            }
+
+            throw new Exception("Não há Enum com esta descrição");
+        }
+
+        public static T ReceberEnumDeValorPadrao<T>(this string valorPadrao)
+        {
+            foreach (MemberInfo field in (MemberInfo[])typeof(T).GetFields())
+            {
+                DefaultValueAttribute[] atributoValorPadrao = (DefaultValueAttribute[])field.GetCustomAttributes(typeof(DefaultValueAttribute), false);
+                if (atributoValorPadrao != null && atributoValorPadrao.Length > 0 && atributoValorPadrao[0].Value.ToString() == valorPadrao)
+                    return (T)Enum.Parse(typeof(T), field.Name);
+            }
+
+            throw new Exception("Não há Enum com este valor padrão");
+        }
+    }
+}
